b317fca46f17621b8b713a39769650284087cc77
tuananhcs <tuananh.cs.159@gmail.com>
2022-02-09 01:28:03 +0700

    first commit

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+import { Form, InputGroup, Col, Row, Container } from 'react-bootstrap'
+import visible from "resources/icons/visible.svg"
+import eye_black from "resources/icons/eye_black.svg";
+import profile from "resources/icons/profile.svg"
+import lock from "resources/icons/lock.svg";
 export default function Profile() {
+    const [isVisible, setisVisible] = useState(false)
+    const [information, setinformation] = useState({
+        Email: "",
+        Password: "",
+        UserName: "",
+        RankAccount: "",
+        Avatar: "",
+        OverView: "",
+        Company: "",
+        Gender: "",
+        Address: "",
+        Birthday: "",
+        Position: "",
+        NewCompany: true,
+        Tenant: "",
+
+    })
     return (
         <div>
-            Profile page
+
+            <h3 class="mt-3 mb-3"> Position:</h3>
+
+            <Form>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        UserName
+                    </Form.Label>
+                    <Col sm="5">
+                        <Form.Control type="text" placeholder="Enter your username" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Password
+                    </Form.Label>
+                    <Col sm="5">
+                        <Form.Control type="text" placeholder="Enter your username" />
+                    </Col>
+                </Form.Group>
+
+                
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Email
+                    </Form.Label>
+                    <Col sm="5">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Birthday
+                    </Form.Label>
+                    <Col sm="5">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Company
+                    </Form.Label>
+                    <Col sm="5">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Position
+                    </Form.Label>
+                    <Col sm="5">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Address
+                    </Form.Label>
+                    <Col sm="5">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+
+            </Form>
         </div>
     )
 }
--- src/pages/Setting/Setting.js
+++ src/pages/Setting/Setting.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+import { Form, InputGroup, Col, Row, Container } from 'react-bootstrap'
+import visible from "resources/icons/visible.svg"
+import eye_black from "resources/icons/eye_black.svg";
+import profile from "resources/icons/profile.svg"
+import lock from "resources/icons/lock.svg";
+import CustomDropdownButton from 'pages/EditReport/components/CustomDropdownButton';
 export default function Setting() {
     return (
         <div>
-            setting pages
+            <h3 class="mt-3 mb-3"> Setting:</h3>
+            <Form.Group as={Row} className="mb-3 align-items-center" controlId="formPlaintextPassword">
+                <Form.Label column sm="2">
+                    Darkmode
+                </Form.Label>
+                <Col sm="5">
+                    <Form.Check
+                        type="switch"
+                        id="custom-switch"
+
+                    />
+                </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3 align-items-center" controlId="formPlaintextPassword">
+                <Form.Label column sm="2">
+                    Language
+                </Form.Label>
+                <Col sm="5">
+                    <CustomDropdownButton title="---" items={["French", "American"]} onClick={(val, index) => {
+
+                    }} />
+                </Col>
+            </Form.Group>
         </div>
     )
 }
