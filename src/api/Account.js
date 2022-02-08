5cbbc359fbfffa1a82b1e5eab561cb81e7fd1e47
tuananhcs <tuananh.cs.159@gmail.com>
2022-02-09 00:25:45 +0700

    full forgot and change passwprd

--- src/api/Account.js
+++ src/api/Account.js
@@ -3,9 +3,13 @@ import axiosClient from "./axios"
 const LoginApi = (data) => {
     return axiosClient.post("/account/login", data)
 }
-const ForgotPassword  = (data) => {
-    console.log(data)
+const ForgotPasswordAPI  = (data) => {
+    console.log("account/forgot?Email="+ data)
     return axiosClient.get("account/forgot?Email=$data" )
+}
+const SetNewPasswordAPI  = (data) => {
+    console.log(data)
+    return axiosClient.post("/account/forgot", data)
 }
 const RegisterApi= (data) => {
     return axiosClient.post("/account/register", data)
@@ -14,9 +18,11 @@ const GoogleLoginApi = (data) => {
     return axiosClient.post("/account/login/google", data)
 }
 
+
 export {
     LoginApi, 
     RegisterApi,
     GoogleLoginApi, 
-    ForgotPassword
+    ForgotPasswordAPI,
+    SetNewPasswordAPI 
 }
