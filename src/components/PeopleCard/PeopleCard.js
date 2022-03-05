6a8724d360bb12300294e22ca860c4d7c5f3909c
hailongmt3102@gmail.com <hailongmt3102@gmail.com>
2022-03-05 10:40:11 +0700

    people card

---
+++ src/components/PeopleCard/PeopleCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../css/TextFont.css'
+import './peopleCard.css'
+
+export default function PeopleCard(props) {
+    return (
+        <div className={props.isManager? "m-4 card manager" : "m-4 card member"}>
+            <div className='d-flex align-items-center'>
+                <img className='m-2' src={props.avatar} />
+                <div className='ms-4'>
+                    <h2>
+                        {props.name}
+                    </h2>
+                    <div>
+                        Email: {props.email}
+                    </div>
+                    <div>
+                        Joining date: {props.joiningDate}
+                    </div>
+                    <div>
+                        Phone number: {props.phone}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
