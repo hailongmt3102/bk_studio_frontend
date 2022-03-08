7e6250e1974728858326e9a37d6dadd3730b998e
tuananhcs <tuananh.cs.159@gmail.com>
2022-03-09 02:30:28 +0700

    fix

--- src/pages/ProjectList/ProjectList.js
+++ src/pages/ProjectList/ProjectList.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { getListProject } from 'api/Project'
 import ProjectBox from '../../components/ProjectBox'
 import NewProjectModel from './components/NewProjectModel'
-
+import { Roboto, Poppins } from "utils/font"
+import { deep_blue_primary } from "../../utils/color"
 const orangeStyle = {
     color: "#FF7F0D",
 }
@@ -27,37 +28,49 @@ export default function ProjectList() {
             )
     }, [appendProject])
 
-    return <div className='m-2 rounded-5 bg-white'>
-        <div className='d-flex flex-row pt-2'>
-            <h1 className='m-2 pe-4'>Project</h1>
-            <button className='btn btn-default shadow bg-body rounded pl-2 pr-2'
-                onClick={() => {
-                    setShowPModel(true)
-                }}
-            >
-                <h1 className='m-0 p-0' style={orangeStyle}>+</h1>
-            </button>
+    return (
+        <div>
+            <div className='d-flex flex-row pt-2'>
 
-            <NewProjectModel
-                show={showPModel}
-                handleClose={() => {
-                    setShowPModel(false)
-                }}
-                onComplete={() => {
-                    setAppendProject(appendProject + 1)
-                }}
-            />
+                <h1 className='m-2 ps-4 pe-3 mt-3' style={{ fontFamily: Poppins, color: deep_blue_primary, fontWeight: "bold", fontSize: "40px" }}>Project</h1>
+                <div className='d-flex flex-row pt-1'>
+
+                    <button className='btn btn-default shadow bg-body rounded pl-2 pr-2 mt-1'
+                        onClick={() => {
+                            setShowPModel(true)
+                        }}
+                    >
+                        <h1 className='m-0 p-0' style={orangeStyle}>+</h1>
+                    </button>
+
+                    <NewProjectModel
+                        show={showPModel}
+                        handleClose={() => {
+                            setShowPModel(false)
+                        }}
+                        onComplete={() => {
+                            setAppendProject(appendProject + 1)
+                        }}
+                    />
+                </div>
+
+            </div>
+
+            <div className='p-4 m-4 rounded-5 bg-white'>
+
+                <div className='row m-4'>
+                    {
+                        projectList.slice(0).reverse().map((project, index) => {
+                            return <div id={index} className="col">
+                                <ProjectBox
+                                    data={project}
+                                />
+                            </div>
+                        })
+                    }
+                </div>
+            </div>
         </div>
-        <div className='row'>
-            {
-                projectList.slice(0).reverse().map((project, index) => {
-                    return <div id={index} className="col">
-                        <ProjectBox
-                            data={project}
-                        />
-                    </div>
-                })
-            }
-        </div>
-    </div>
+
+    )
 }
