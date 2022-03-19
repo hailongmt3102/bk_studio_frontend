8a48742151f4690df21cbfe3bccf9efe2e45b208
hailongmt3102@gmail.com <hailongmt3102@gmail.com>
2022-03-19 13:07:09 +0700

    pending

--- src/pages/ImportData/Components/Table.js
+++ src/pages/ImportData/Components/Table.js
@@ -1,45 +1,43 @@
 import React from 'react'
+import './table.css'
 
 export default function Table(props) {
     return (
         <div className='mt-3'>
             <h4 className='m-auto'>{props.name}</h4>
-            <table class="table table-info table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        {
-                            props.columns.data.map((col, index) => {
-                                return (
+            <div>
+                <table class='custom-table'>
+                    <thead>
+                        <tr>
+                            {
+                                props.columns.data.map((col, index) =>
                                     props.columns.active[index] ?
-                                        <th scope='col'>{col}</th>
+                                        <th>{col}</th>
                                         : null
                                 )
-                            })
-                        }
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        props.rows.map((ele, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th scope="row">{index + 1}</th>
-                                    {
-                                        props.columns.data.map((col, index) => {
-                                            return (
+                            }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.rows.map((ele, index) => {
+                                return (
+                                    <tr key={index}>
+                                        {
+                                            props.columns.data.map((col, index) =>
                                                 props.columns.active[index] ?
                                                     <td>{props.data[ele][col]}</td>
                                                     : null
+
                                             )
-                                        })
-                                    }
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
+                                        }
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
