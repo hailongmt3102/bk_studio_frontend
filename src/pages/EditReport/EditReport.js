0202b069a6b4cc7235ab9978401fcceeb90d73c1
tuananhcs <tuananh.cs.159@gmail.com>
2022-03-19 11:27:13 +0700

    commit icon

--- src/pages/EditReport/EditReport.js
+++ src/pages/EditReport/EditReport.js
@@ -6,9 +6,22 @@ import "./EditReport.css";
 import { Tabs, Tab } from 'react-bootstrap-tabs';
 import { Form, InputGroup, Col, Button, FormControl } from 'react-bootstrap'
 import back from "resources/icons/back_round_deep_blue.svg"
+import align_left from "resources/icons/align_left.svg"
+import align_right from "resources/icons/align_right.svg"
+import align_center from "resources/icons/align_center.svg"
+import align_justify from "resources/icons/align_justify.svg"
+import bold from "resources/icons/bold.svg"
+import underline from "resources/icons/underline.svg"
+import italic from "resources/icons/italic.svg"
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import { GetTableColumns, QueryData } from "api/DataSources"
 import BootstrapSelect from 'react-bootstrap-select-dropdown';
 import { Roboto, Poppins } from "../../utils/font"
+
+
+
+
 
 export default function EditReport() {
     // this function will be call when loaded page
@@ -85,21 +98,8 @@ export default function EditReport() {
     }
     const [key, setKey] = useState('Data');
 
-
-
-    const [options, setOptions] = useState([
-        {
-            "labelKey": "optionItem1",
-            "value": "Option item 1"
-        },
-        {
-            "labelKey": "optionItem2",
-            "value": "Option item 2"
-        }
-    ])
-    const handleChange = (selectedOptions) => {
-        console.log(selectedOptions);
-    }
+    const fonts = ['Roboto', 'Poppins'];
+    const size = ['14', '16', "32", '45'];
 
     // render components
     return (
@@ -109,9 +109,103 @@ export default function EditReport() {
                 <div className="col-2 ">
                     <Tabs className="p-2" activeKey={key} onSelect={(k) => setKey(k)}>
                         <Tab className="p-4" eventKey="Data" label="Data">
-                            <BootstrapSelect options={options} onChange={handleChange} />
+                            <h4>Script</h4>
+                            <div className="m-4">SELECT* FROM </div>
+                            <div className="mt-3">
+                                <h4>Data sources:</h4>
+                            </div>
+                            <div className="row mt-5">
+                                <div className="col">
+                                    <h4>Field:</h4>
+                                </div>
+                                <div className="col">
+                                    <h4>Type</h4>
+                                </div>
+                            </div>
                         </Tab>
-                        <Tab className="p-4" eventKey="Format" label="Style">Tab 2 content</Tab>
+                        <Tab className="p-4" eventKey="Format" label="Style">
+                            <div className="mt-2">Text</div>
+                            <div className="row mt-2">
+                                <div className="col">
+                                    <Autocomplete
+                                        id="custom-input-demo"
+                                        options={fonts}
+                                        renderInput={(params) => (
+                                            <div ref={params.InputProps.ref}>
+                                                <input type="text" {...params.inputProps} />
+                                            </div>
+                                        )}
+                                    />
+                                </div>
+                            </div>
+                            <div className="mt-2">Size</div>
+                            <div className="row mt-2">
+                                <div className="col">
+                                    <Autocomplete
+                                        id="custom-input-demo"
+                                        options={size}
+                                        renderInput={(params) => (
+                                            <div ref={params.InputProps.ref}>
+                                                <input type="text" {...params.inputProps} />
+                                            </div>
+                                        )}
+                                    />
+                                </div>
+                            </div>
+
+
+                            <div className="row m-2 m-0 p-0 mt-2  h-100  align-items-center">
+                                <div className="col-1 m-0 p-0 text-center" >
+                                    <button type="button" class="btn btn-sm ms-2 p-2" onClick={() => { }}><img src={bold} height="15px" width="15px" /></button>
+                                </div>
+                                <div className="col-1 m-0 p-0 ms-5" onClick={() => { }}>
+                                    <button type="button" class="btn m-0 p-0 btn-sm ms-2" onClick={() => { }}> <img src={italic} height="30px" width="30px" /></button>
+                                </div>
+                                <div className="col-1 m-0 p-0 ms-5" onClick={() => { }}>
+                                    <button type="button" class="btn m-0 p-0 btn-sm ms-2" onClick={() => { }}> <img src={underline} height="20px" width="20px" /></button>
+                                </div>
+
+                            </div>
+                            <div>Alignmnet</div>
+                            <div className="row m-3">
+                                <div className="col">
+                                    <img src={align_left} height="20px" width="20px" />
+                                </div>
+                                <div className="col">
+                                    <img src={align_right} height="20px" width="20px" />
+                                </div>
+                                <div className="col">
+                                    <img src={align_center} height="20px" width="20px" />
+                                </div>
+                                <div className="col">
+                                    <img src={align_justify} height="20px" width="20px" />
+                                </div>
+
+                            </div>
+                            <div>Fill</div>
+                            <Autocomplete
+                                id="custom-input-demo"
+                                options={size}
+                                renderInput={(params) => (
+                                    <div ref={params.InputProps.ref}>
+                                        <input type="text" {...params.inputProps} />
+                                    </div>
+                                )}
+                            />
+
+                            <div>Stroke</div>
+                            <Autocomplete
+                                id="custom-input-demo"
+                                options={size}
+                                renderInput={(params) => (
+                                    <div ref={params.InputProps.ref}>
+                                        <input type="text" {...params.inputProps} />
+                                    </div>
+                                )}
+                            />
+
+                        </Tab>
+
                     </Tabs>
                 </div>
                 <div className="col-10 h-200">
