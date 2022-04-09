39af18f8dce8e2d8a4c171f98caa08e1e3628a86
hailongmt3102@gmail.com <hailongmt3102@gmail.com>
2022-04-09 21:39:23 +0700

    Localization init

---
+++ src/utils/localizationData.js
@@ -0,0 +1,36 @@
+const data = {
+    English: {
+        signIn: "Sign in",
+        signInIf: "If you don't have an account register",
+        youCan: "You can",
+        registerHere: "Register here !",
+        Email: "Email",
+        Password: "Password",
+        EnterEmail: "Enter your email address",
+        EnterPassword : "Enter your email password",
+        RememberMe : "Remember me",
+        Login : "Login",
+        ForgotPass : "Forgot Password !",
+        OrContinue: "or continue with",
+        GGAccount : "Google account"
+    },
+    Vietnamese: {
+        signIn: "Đăng nhập",
+        signInIf: "Nếu bạn chưa có tài khoản",
+        youCan: "Bạn có thể",
+        registerHere: "Đăng kí ngay !",
+        Email: "Email",
+        Password: "Mật khẩu",
+        EnterEmail: "Nhập email của bạn",
+        EnterPassword : "Nhập mật khẩu",
+        RememberMe : "Nhớ mật khẩu",
+        Login : "Đăng nhập",
+        ForgotPass : "Quên mật khẩu !",
+        OrContinue: "hoặc tiếp tục với",
+        GGAccount : "tài khoản Google"
+    }
+};
+
+export {
+    data
+}
