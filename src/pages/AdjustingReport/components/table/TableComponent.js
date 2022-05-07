c04cfa79d776b15ec858fc944e6160dcef6b9e61
hailongmt3102@gmail.com <hailongmt3102@gmail.com>
2022-05-07 11:17:39 +0700

    table in report

--- src/pages/AdjustingReport/components/table/TableComponent.js
+++ src/pages/AdjustingReport/components/table/TableComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Rnd } from 'react-rnd'
 import './TableComponent.css'
 import {
@@ -7,40 +7,57 @@ import {
 } from '@mui/x-data-grid'
 import { useDemoData } from "@mui/x-data-grid-generator";
 const selectReg = /select.*(?= from)/
+
+
+function CustomToolbar() {
+    return (
+        <GridToolbarContainer>
+            <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
+        </GridToolbarContainer>
+    );
+}
+
 export default function TableComponent(props) {
 
-    // console.log("propsdatadata[0]", props.data.data[0])
-    // console.log("propsdata[0]", props.data)
+
     const [rows, setRows] = useState([])
     const [columns, setColumns] = useState([])
-    useEffect(() => {
-        // let selectArray = (selectReg.exec(props.data.QueryCommand) ?? [""])[0].replace("select ", "").split(",").map(ele => ele.trim())
-        // setColumns(selectArray)
-        const width = 150
-        const keys = Object.keys(props.data.data[0])
+    const [visibleId, setVisibleId] = useState(true)
 
+    const [columnList, setColumnList] = useState([])
+    useEffect(() => {
+        let selectArray = (selectReg.exec(props.data.QueryCommand) ?? [""])[0].replace("select ", "").split(",").map(ele => ele.trim())
+        setColumnList(selectArray)
+
+        const keys = Object.keys(props.data.data[0])
         let columns = keys.map((key) => {
             return {
                 field: key,
                 headerName: key,
                 editable: false,
-                width: width
             }
         })
 
         setColumns(columns)
+        let rows
+        if (!keys.includes('id')) {
+            rows = props.data.data.map((row, index) => {
+                return { ...row, id: index }
+            })
+            setVisibleId(false)
+        }
+        else rows = props.data.data
 
-
-        let rows = props.data.data.map((row, index) => {
-            return { ...row, id: index }
-        })
-        console.log("row", rows)
         setRows(rows)
     }, [props.data.data])
+
+    const optionRef = useRef()
+
     function CustomToolbar() {
         return (
-            <GridToolbarContainer>
-                <GridToolbarExport />
+            <GridToolbarContainer >
+                <GridToolbarExport ref={optionRef} style={{ visibility: 'hidden' }} />
+                <h4>{props.data.Title}</h4>
             </GridToolbarContainer>
         );
     }
@@ -51,82 +68,75 @@ export default function TableComponent(props) {
     });
 
     return (
-        <Rnd
-            disableDragging={props.disableDragging}
-            enableResizing={props.enableResizing}
-            size={{ width: props.data.Width, height: props.data.Height }}
-            position={{ x: props.data.Position.x, y: props.data.Position.y }}
-            onDragStop={(e, d) => {
-                props.updateDataTable({
-                    ...props.data, Position: {
-                        x: d.x,
-                        y: d.y
-                    }
-                })
+        <div
+            onClick={(e) => {
+                console.log("asdf")
+                console.log(optionRef.current.contains(e.target))
             }}
-            onResizeStop={(e, direction, ref, delta, position) => {
-                props.updateDataTable({ ...props.data, Width: ref.style.width, Height: ref.style.height })
-            }}
-            className={`${props.classstyle} component-container`}
         >
-            {
-                props.data.data !== undefined && props.data.data.length == 0 &&
-                <div>
-                    <h4 className='ms-2'>{props.data.Title}</h4>
-                    <table class='component-table'>
-                        <thead>
-                            <tr>
-                                {
-                                    columns.map(ele => <th>{ele}</th>)
-                                }
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            }
-            {
-                props.data.data !== undefined && props.data.data.length > 0 ?
-                    <div>
-                        <h4 className='ms-2'>{props.data.Title}</h4>
-                        {/* <table class='component-table'>
-                            <thead>
-                                <tr>
-                                    {
-                                        Object.keys(props.data.data[0]).map(ele => <th>{ele}</th>)
-                                    }
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    props.data.data.map((ele, index) =>
-                                        <tr key={index}>
-                                            {
-                                                Object.values(ele).map((value, i) =>
-                                                    <td>{value}</td>
-                                                )
-                                            }
-                                        </tr>
-                                    )
-                                }
-                            </tbody>
-                        </table> */}
-                        <DataGrid
-                            loading={loading}
-                            components={{
-                                Toolbar: CustomToolbar
-                            }}
-                            rows={rows}
-                            columns={columns}
-                            columnVisibilityModel={{
-                                id: true,
-                            }}
-                            experimentalFeatures={{ newEditingApi: true }}
-                        // editMode="cell"
-                        // onCellEditStop={handleRowEditStop}
-                        />
-                    </div>
-                    : null
-            }
-        </Rnd>
+
+            <Rnd
+                disableDragging={props.disableDragging}
+                enableResizing={props.enableResizing}
+                size={{ width: props.data.Width, height: props.data.Height }}
+                position={{ x: props.data.Position.x, y: props.data.Position.y }}
+                onDragStop={(e, d) => {
+                    props.updateDataTable({
+                        ...props.data, Position: {
+                            x: d.x,
+                            y: d.y
+                        }
+                    })
+                }}
+                onResizeStop={(e, direction, ref, delta, position) => {
+                    props.updateDataTable({ ...props.data, Width: ref.style.width, Height: ref.style.height })
+                }}
+                className={props.classstyle}
+            >
+
+                {
+                    props.data.data !== undefined && props.data.data.length == 0 ?
+                        <div>
+                            <h4 className='ms-2'>{props.data.Title}</h4>
+                            <table class='component-table'>
+                                <thead>
+                                    <tr>
+                                        {
+                                            columnList.map(ele => <th>{ele}</th>)
+                                        }
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                        :
+                        <div style={{ height: '100%', width: '100%' }} >
+                            <div className='text-start'>
+                                <button
+                                    className='btn btn-default'
+                                    onClick={() => {
+                                        optionRef.current.click()
+                                    }}
+                                >
+                                    Export
+                                </button>
+                            </div>
+                            <DataGrid
+                                rows={rows}
+                                columns={columns}
+                                columnVisibilityModel={{
+                                    id: visibleId,
+                                }}
+                                components={{
+                                    Toolbar: CustomToolbar,
+                                }}
+                                experimentalFeatures={{ newEditingApi: true }}
+                                loading={loading}
+                            />
+                        </div>
+                }
+
+            </Rnd>
+        </div>
+
     )
 }
