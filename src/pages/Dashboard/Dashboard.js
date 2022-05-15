8cf6d601e6aaba98fee081054225831a64442f88
hailongmt3102@gmail.com <hailongmt3102@gmail.com>
2022-05-15 11:11:50 +0700

    brown card

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -49,7 +49,7 @@ export default function Dashboard() {
     return (
         <div className='m-0 ps-4 pe-4'>
             <div className='mt-2 mb-2 customforeground'>
-                <h1 className='ms-4 mt-2 PrimaryFontColor' style={{"fontWeight": "bold", fontSize: "40px" }}>{localization.Project}</h1>
+                <h1 className='ms-4 mt-2 PrimaryFontColor' style={{ "fontWeight": "bold", fontSize: "40px" }}>{localization.Project}</h1>
                 <ScrollMenu>
                     {projectList.map((ele, index) => (
                         <div className='col ms-5 mb-4 mt-4'>
@@ -61,24 +61,27 @@ export default function Dashboard() {
                     ))}
                 </ScrollMenu>
             </div>
-            <div className='mt-2 mb-2 customforeground row' >
-                <div className='col-7 mt-4' >
-                    <h1 className='m-0 pt-4 ms-4 p-0 PrimaryFontColor' style={{ "fontWeight": "bold", fontSize: "40px" }}>{localization.Templates}</h1>
-                    <p className='m-0 ms-4 p-0'>Variety template for your choice</p>
+            <div className='mt-2 mb-2  row' >
+                <div className='col-7 m-0' style={{height: "100%"}} >
+                    <div className='m-2 ms-1 customforeground'>
+                        <h1 className='m-0 pt-4 ms-4 p-0 PrimaryFontColor' style={{ "fontWeight": "bold", fontSize: "40px" }}>{localization.Templates}</h1>
+                        <p className='m-0 ms-4 p-0'>Variety template for your choice</p>
 
-                    <div class="row justify-content-center m-0 p-0">
-                        {reports.map(ele =>
-                            <div
-                                className='col-lg mt-4 m-0 p-0'
-                                style={{ "minWidth": "300px", "maxWidth": "300px" }}
-                            >
-                                <TemplateMiniCard data={ele} type="Templates" />
-                            </div>
-                        )}
+                        <div class="row justify-content-center m-0 p-0">
+                            {reports.map(ele =>
+                                <div
+                                    className='col-lg mt-4 m-0 p-0'
+                                    style={{ "minWidth": "300px", "maxWidth": "300px" }}
+                                >
+                                    <TemplateMiniCard data={ele} type="Templates" />
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div >
-                <div className='col-5  m-0 mt-4 p-0 '>
-                    <h1 className='m-0 mt-4 ms-4 p-0 PrimaryFontColor' style={{ "fontWeight": "bold", fontSize: "40px" }}>{localization.People}</h1>
+                <div className='col-5 m-0 p-0 '>
+                    <div className='m-2 ms-1 pt-4 customforeground'>
+                    <h1 className='m-0 ms-4 p-0 PrimaryFontColor' style={{ "fontWeight": "bold", fontSize: "40px" }}>{localization.People}</h1>
                     <div className='row ms-2'>
                         <div class="container m-0 p-0">
                             <div class="row mt-3 ">
@@ -95,6 +98,7 @@ export default function Dashboard() {
                                 }
                             </div>
                         </div>
+                    </div>
                     </div>
                 </div>
 
