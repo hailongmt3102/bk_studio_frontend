218c5303c2273a1d1820e9905f8588c173e89884
tuananhcs <tuananh.cs.159@gmail.com>
2022-05-19 13:24:13 +0700

    fix avatar

--- src/components/PeopleCardMini/PeopleCardMini.js
+++ src/components/PeopleCardMini/PeopleCardMini.js
@@ -2,17 +2,20 @@ import React from 'react'
 
 
 import '../PeopleCard/peopleCard.css'
-
+import Avatar from '@mui/material/Avatar';
 
 import people_default from "resources/icons/people_default.svg"
 export default function PeopleCardMini(props) {
     return (
         <div className='row justify-content-center m-auto level1 p-1'>
             <div className='col-2 '>
-                {props.avatar === "" ?
-                    <img src={people_default} height="50px" width="50px" />
-                    : <img src={props.avatar} height="50px" width="50px" style={{ "border-radius": "50%" }} />
-                }
+                <Avatar sx={{ bgcolor: "#FFFFFF" }}>
+                    {props.avatar === "" ?
+                        <img src={people_default} height="50px" width="50px" />
+                        : <img src={props.avatar} height="50px" width="50px" />
+                    }
+                </Avatar>
+
             </div>
             <div className='col-10 overflow' >
                 <h5 className=' ms-4 customFontRoboto' style={{ fontSize: "20px", fontWeight: "bold" }}>
